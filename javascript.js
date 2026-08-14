fb55e6f101fcab617875192edde3d4bc3c045dd7
Felicity <22337@westlakegirls.school.nz>
2026-08-14 14:14:24 +1200

    Added footer

--- javascript.js
+++ javascript.js
@@ -1,12 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyARHYky_cakOuuEjtCAbGoUKHWCG0fqWM4",
-  authDomain: "ways-to-touch-grass.firebaseapp.com",
-  projectId: "ways-to-touch-grass",
-  storageBucket: "ways-to-touch-grass.firebasestorage.app",
-  messagingSenderId: "511870144078",
-  appId: "1:511870144078:web:520377d66af9f650a13d1f",
-  measurementId: "G-SG11X8K06T"
-};
 
 function openMenu() {
     menuTest.style.display = 'initial';
@@ -28,48 +19,6 @@ function reset() {
 menuBox.onmouseover = openMenu;
 menuBarss.onmouseleave = reset;
 
-if (document.getElementById('signIn')) {
-
-function openMenu() {
-    menuTest.style.display = 'initial';
-    menuTitle.style.display = 'initial';
-    menuBox.style.display = 'none';
-    menuTitles.style.display = 'none';    
-    menuBarss.style.display = 'initial';    
-    
-}
-
-function reset() {
-    menuBox.style.display = 'initial';
-    menuTitles.style.display = 'initial';
-    menuTest.style.display = 'none';
-    menuTitle.style.display = 'none';
-    menuBarss.style.display = 'none';    
-}
-
-menuBox.onmouseover = openMenu;
-menuBarss.onmouseleave = reset;
-let signupBtn = document.getElementById("signupBtn");
-let signinBtn = document.getElementById("signinBtn");
-let nameField = document.getElementById("nameField");
-let title = document.getElementById("title");
-
-signinBtn.onclick = function(){
-    nameField.style.maxHeight = "0";
-    title.innerHTML = "Sign In";
-    signupBtn.classList.add("disable")
-    signinBtn.classList.remove("disable")
-}
-
-signupBtn.onclick = function(){
-    nameField.style.maxHeight = "60px";
-    title.innerHTML = "Sign Up";
-    signupBtn.classList.remove("disable")
-    signinBtn.classList.add("disable")
-}
-
-
-}
 
 if (document.getElementById('frontPage')) {
 
