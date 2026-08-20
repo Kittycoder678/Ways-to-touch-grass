a2dbfcb25fdbb583730c6fb70a39549168e17ed3
Felicity <22337@westlakegirls.school.nz>
2026-08-21 10:23:54 +1200

    Finshed cooking and arts page did most of sizing, my menubar text still does not work.

--- javascript.js
+++ javascript.js
@@ -1,40 +1,40 @@
+// Get elements safely
+const menuBox = document.getElementById('menuBox');
+const menuTitles = document.getElementById('menuTitles');
+const menuTest = document.getElementById('menuTest');
+const menuTitle = document.getElementById('menuTitle');
+const menuBarss = document.getElementById('menuBarss');
 
+// Open menu
 function openMenu() {
     menuTest.style.display = 'initial';
     menuTitle.style.display = 'initial';
     menuBox.style.display = 'none';
-    menuTitles.style.display = 'none';    
-    menuBarss.style.display = 'initial';    
-    
+    menuTitles.style.display = 'none';
+    menuBarss.style.display = 'initial';
 }
 
+// Close menu
 function reset() {
     menuBox.style.display = 'initial';
     menuTitles.style.display = 'initial';
     menuTest.style.display = 'none';
     menuTitle.style.display = 'none';
-    menuBarss.style.display = 'none';    
+    menuBarss.style.display = 'none';
 }
 
+// Hover open / leave close
 menuBox.onmouseover = openMenu;
-menuBarss.onmouseleave = reset;
+menuBarss.onmouseleave = reset;
 
-
+// Front page button navigation
 if (document.getElementById('frontPage')) {
 
-cookingBtn.onclick = function(){
-    document.location='cooking.html'
-}
+    const cookingBtn = document.getElementById('cookingBtn');
+    const readingBtn = document.getElementById('readingBtn');
+    const artsBtn = document.getElementById('artsBtn');
 
-musicBtn.onclick = function(){
-    document.location='music.html'
-}
-
-readingBtn.onclick = function(){
-    document.location='reading.html'
-}
-
-artsBtn.onclick = function(){
-    document.location='arts.html'
-}
+    cookingBtn.onclick = () => document.location = 'cooking.html';
+    readingBtn.onclick = () => document.location = 'reading.html';
+    artsBtn.onclick = () => document.location = 'arts.html';
 }
